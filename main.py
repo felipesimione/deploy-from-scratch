349ec80f9cbc66ef07172e908c616abea7e6932d
Felipe Simione <felipesimione@gmail.com>
2025-05-07 20:35:23 -0300

    Reestruturacao das pastas e códigos para versão inicial 0.1

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from typing import List, Dict
 
 app = FastAPI()
+
+produtos: List[Dict[str, str]] = [
+    {"id": 1, "nome": "Smartphone", "preco": 1000.00},
+    {"id": 2, "nome": "Notebook", "preco": 3000.00},
+    {"id": 3, "nome": "Fone de ouvido", "preco": 30.00},
+]
 
 @app.get("/")
 def ola_mundo():
     return {"message": "Olá, mundo!"}
+
+@app.get("/produtos")
+def listar_produtos():
+    return produtos
