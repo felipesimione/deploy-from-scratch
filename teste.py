f5c7588ee8e50e9f3044b666cf40a28a07fe019a
Felipe Simione <felipesimione@gmail.com>
2025-05-07 20:45:00 -0300

    adicionado novo endpoint de produto - pytest

--- teste.py
+++ teste.py
@@ -12,3 +12,11 @@ def test_ola_mundo_json():
     response = client.get("/")
     assert response.json() == {"message": "Olá, mundo!"}
 
+def test_listar_produtos_status_code():
+    response = client.get("/produtos")
+    assert response.status_code == 200
+
+def test_tamanho_da_lista_de_produtos():
+    response = client.get("/produtos")
+    assert len(response.json()) == 3
+
